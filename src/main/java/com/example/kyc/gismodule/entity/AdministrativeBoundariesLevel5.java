c3fe766db2bd3fc32ea3320838a037cd62dea94c
zakaria.ahmmed <zakaria.ahmmed@bracits.com>
2026-04-23 04:09:11 +0600

    location search fixed

--- src/main/java/com/example/kyc/gismodule/entity/AdministrativeBoundariesLevel5.java
+++ src/main/java/com/example/kyc/gismodule/entity/AdministrativeBoundariesLevel5.java
@@ -10,7 +10,7 @@ import org.hibernate.annotations.GenericGenerator;
 import java.util.UUID;
 
 @Entity
-@Table(name = "administrative_boundaries_level_6", schema = "public")
+@Table(name = "administrative_boundaries_level_5", schema = "public")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
